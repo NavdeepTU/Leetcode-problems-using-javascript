4ace9142351148cca78609b39648b95bac7a596b
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-07-25 09:29:04 +0530

    first unique character in a string

---
+++ 387. First Unique Character in a String.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    let freq_hm = new Map();
+    let idx_hm = new Map();
+    let min_idx = Infinity;
+    for(let idx=0; idx<s.length; idx++) {
+        if(freq_hm.has(s[idx])) {
+            let freq = freq_hm.get(s[idx]);
+            freq_hm.set(s[idx], freq+1);
+        }else {
+            freq_hm.set(s[idx], 1);
+            idx_hm.set(s[idx], idx);
+        }
+    }
+    
+    for(let char of freq_hm.keys()) {
+        if(freq_hm.get(char) === 1) {
+            if(idx_hm.get(char) < min_idx) {
+                min_idx = idx_hm.get(char);
+            }
+        }
+    }
+    
+    if(!isFinite(min_idx))
+        min_idx = -1;
+    
+    return min_idx;
+};
