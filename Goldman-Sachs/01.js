77712904619f399733e7c4fe69360c67ab1ea83e
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-07-26 09:22:28 +0530

    ngram^?

--- Goldman-Sachs/01.js
+++ Goldman-Sachs/01.js
@@ -1,17 +1,19 @@
 // 2nd smallest element of a sorted rotated array. E.g. 5,6,1,2,3,4
 
 
-let findSecondMin = function(nums) {
+let findSecondMin = function (nums) {
     let left = 0;
-    let right = nums.length-1;
-    while(left < right) {
-        let mid = Math.floor((left + right)/2);
-        if(nums[mid] > nums[right]) {
-            left = mid+1;
-        }else
+    let right = nums.length - 1;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        // if nums[mid] > nums[right], that means we need to move right to find the smallest element
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else
             right = mid;
     }
-    return nums[(left+1)%nums.length];
+    // left index has the smallest element but we need second smallest hence we increment in circular fation
+    return nums[(left + 1) % nums.length];
 };
 
 let arr = [4, 5, 6, 7, 0, 1, 2];
